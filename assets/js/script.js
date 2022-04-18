6a75010fb1cf76a8fedaaca0108d217b8b9fa2e0
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-18 08:03:37 +0000

    Addfunction for tiles to become hidden and visible

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,27 @@ function startGame() {
 }
 
 var loop = 0
+var tileVisible = false;
 
 function gameTime() {
+    tileVisible = !tileVisible;
     loop++;
     if (loop < 12) {
         setTimeout(gameTime, 3000);
     } else {
         alert("Game Over..!")
-        }
     }
+}
+
+function flashTile() {
+    var  board = document.getElementById("game-board");
+    var classToSet = "";
+    if(tileVisible) {
+        classToSet = "tile visible";
+    } else {
+        classToSet = "tile hidden";
+    }
+    for(var i = 0; i < 6; i++) {
+        board.children[i].className = classToSet;
+    }
+}
