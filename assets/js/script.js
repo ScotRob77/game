a80d9baf1caef5da1d96fd15c89ba372caa27a85
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-18 07:50:02 +0000

    Add game tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 function startGame() {
-    
+    gameTime();
 }
+
+var loop = 0
+
+function gameTime() {
+    loop++;
+    if (loop < 12) {
+        setTimeout(gameTime, 3000);
+    } else {
+        alert("Game Over..!")
+        }
+    }
