f29948c968ae3fd4284d4958d30c5595f7466d2d
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-18 10:09:38 +0000

    Add score function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function gameTime() {
     if (loop < 12) {
         setTimeout(gameTime, 1500);
     } else {
-        alert("Game Over..!")
+        alert("You scored " + score)
     }
 }
 
@@ -33,7 +33,13 @@ function createHeroesVillans() {
     var  board = document.getElementById("game-board");
     for(var i = 0; i < 6; i++) {
     board.children[i].innerHTML = "Hero";
+    board.children[i].onclick = function() {
+        score += -2;
+    }
     }
     var randomHero = Math.floor(Math.random() * 6) + 1;
     board.children[randomHero-1].innerHTML = "Villain";
+    board.children[randomHero-1].onclick = function() {
+        score++;
+    }
 }
